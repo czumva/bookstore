d22894d0db2951812c5d025bdeda58ff64e98a8e
carloszumva@gmail.com <carloszumva@gmail.com>
2018-04-21 16:33:05 -0500

    Introduccion a VueJS Complementos

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyBh7JitwCt5Vz3CbTFZsJRirZ1Uglzm3vg",
+    authDomain: "bookstore-bc975.firebaseapp.com",
+    databaseURL: "https://bookstore-bc975.firebaseio.com",
+    projectId: "bookstore-bc975",
+    storageBucket: "bookstore-bc975.appspot.com",
+    messagingSenderId: "632887318950"
+
+};
